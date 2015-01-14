84fef8144e8dd7847127e11f5b80604891295e27
Mark Chapman <mark.chapman@gmail.com>
2015-01-14 20:47:33 +0000

    Rename the directive

--- fng-ui-date.js
+++ fng-ui-date.js
@@ -1,6 +1,6 @@
 // See https://github.com/angular-ui/bootstrap/issues/2659
 
-formsAngular.directive('uiDate', function (){
+formsAngular.directive('datepickerPopupFix', function (){
   return {
     restrict: 'EAC',
     require: 'ngModel',
